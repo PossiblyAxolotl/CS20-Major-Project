60f162f3e25e1dbdb1c192c9df8090a4ad1f6aba
PossiblyAxolotl <possiblyaxolotl@gmail.com>
2023-12-08 14:56:09 -0600

    remove progmem

--- data/player_consts.h
+++ data/player_consts.h
@@ -1,4 +1,4 @@
-const int PROGMEM player_directions[3][3] = { // temp values as not all art is made yet
+const int player_directions[3][3] = { // temp values as not all art is made yet
   {2,2,2},
   {1,1,3},
   {0,0,0}
